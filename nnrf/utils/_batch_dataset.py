7785b8d5d3d9e0f1b0fc0a45e230f204cc3de782
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-05-17 18:57:18 -0400

    Add n_batches and fix organize

--- nnrf/utils/_batch_dataset.py
+++ nnrf/utils/_batch_dataset.py
@@ -41,8 +41,9 @@ class BatchDataset:
 		self.X = np.array(X)
 		self.Y = np.array(Y) if Y is not None else None
 		self.seed = create_random_state(seed=seed)
-		self.available = np.arange(len(self.X))
+		self.available = np.arange(len(self.X)).astype(int)
 		self.batch_size = 1
+		self.n_batches = self._calculate_n_batches()
 		self.order = []
 		self.i = 0
 
@@ -57,9 +58,11 @@ class BatchDataset:
 			Batch size.
 		"""
 		self.batch_size = batch_size
+		self.n_batches = self._calculate_n_batches()
 		self.order = [op for op in self.order if op != 'batch']
 		self.order.append('batch')
 		self.i = 0
+		return self
 
 	def repeat(self):
 		"""
@@ -68,6 +71,7 @@ class BatchDataset:
 		self.order = [op for op in self.order if op != 'repeat']
 		self.order.append('repeat')
 		self.i = 0
+		return self
 
 	def shuffle(self):
 		"""
@@ -76,6 +80,7 @@ class BatchDataset:
 		self.order = [op for op in self.order if op != 'shuffle']
 		self.order.append('shuffle')
 		self.i = 0
+		return self
 
 	def next(self):
 		"""
@@ -90,22 +95,33 @@ class BatchDataset:
 			The batched data, and if available, labels.
 		"""
 		if self.i == 0 : self._organize()
-		if len(self.available) <= 2 * self.batch_size:
-			if len(self.available) < self.batch_size:
-				return [], []
-			batch = self.available[:batch_size]
-			self.available = self.available[batch_size:]
-			self._organize(prepend=self.available)
+		if len(self.available) < self.batch_size:
+			batch = self.available
+			self.available = np.array([])
 		else:
-			batch = self.available[:batch_size]
-			self.available = self.available[batch_size:]
-		i += 1
+			batch = self.available[:self.batch_size]
+			self.available = self.available[self.batch_size:]
+		if len(self.available) < self.batch_size:
+			self._organize(prepend=self.available)
+		self.i += 1
 		if self.Y is None:
 			return self.X[batch]
 		else:
-			return (self.X[batch], self.Y[batch].reshape(-1))
+			return self.X[batch], self.Y[batch]
 
-	def _organize(prepend=[], append=[]):
+	def _calculate_n_batches(self):
+		"""
+		Calculate the number of batches that cover
+		all data in the dataset.
+
+		Returns
+		-------
+		n_batches : int
+			Number of batches.
+		"""
+		return len(self.X) // self.batch_size + 1
+
+	def _organize(self, prepend=[], append=[]):
 		"""
 		Organize the BatchDataset according to `order`.
 		In this manner, the order of shuffle, repeat, and
@@ -121,27 +137,29 @@ class BatchDataset:
 			Append these indices to the reorganized list.
 			Data drawn will exhaust this list last.
 		"""
-		try : shuffle = np.argwhere(self.order == 'shuffle').flatten()[0]
+		order = np.array(self.order)
+		try : shuffle = np.argwhere(order == 'shuffle').flatten()[0]
 		except : shuffle = np.inf
-		try : repeat = np.argwhere(self.order == 'repeat').flatten()[0]
+		try : repeat = np.argwhere(order == 'repeat').flatten()[0]
 		except : repeat = np.inf
-		try : batch = np.argwhere(self.order == 'batch').flatten()[0]
+		try : batch = np.argwhere(order == 'batch').flatten()[0]
 		except : batch = np.inf
+		length_Y = len(self.Y)
 		if shuffle < repeat and shuffle < batch:
-			self.available = np.arange(len(X))
+			self.available = np.arange(length_Y)
 			self.seed.shuffle(self.available)
 		elif repeat < shuffle:
 			if shuffle < batch:
-				self.available = self.seed.choice(np.arange(len(Y)), len(Y))
+				self.available = self.seed.choice(np.arange(length_Y), length_Y)
 			elif batch < shuffle and shuffle < np.inf:
-				self.available = np.arange(len(X)).reshape(-1,self.batch_size)
+				self.available = np.arange(length_Y).reshape(-1,self.batch_size)
 				n_batches = np.arange(len(self.available))
 				indices = self.seed.choice(n_batches, len(self.available))
 				self.available = self.available[indices].flatten()
 			else:
-				self.available = np.arange(len(X))
+				self.available = np.arange(length_Y)
 		elif batch < shuffle < repeat:
-			self.available = np.arange(len(X)).reshape(-1,self.batch_size)
+			self.available = np.arange(length_Y).reshape(-1,self.batch_size)
 			self.seed.shuffle(self.available)
 			self.available.flatten()
-		self.available = np.concatenate((prepend, self.available, append), axis=0)
+		self.available = np.concatenate((prepend, self.available, append), axis=0).astype(int)
